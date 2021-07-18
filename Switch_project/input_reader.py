d6a9b7dc4c053c042ee8c625a36ae14dc88f53bd
Schroedo1994 <dominik.schroeder94@gmail.com>
2021-07-18 15:35:10 +0200

    recent code update

--- Switch_project/input_reader.py
+++ Switch_project/input_reader.py
@@ -1,9 +1,6 @@
-# TODO: implement csv reader to create dict from data source
-
 # Import algorithm classes
 from bfgs import BFGS
 from mlsl import MLSL
-from ea import EA
 from pso import PSO
 from cmaes import CMAES
 from de import DE
@@ -18,8 +15,8 @@ def create_dict():
 
     """
     data = {
-      25.1: CMAES, 
-      1e-8: BFGS 
+      1e-2: CMAES, 
+      1e-8: BFGS
     }
 
     return data
